534739014266d8d8c00bdfb7043550f21bfc5945
Vuong Tran <tranduyvuong100@gmail.com>
2025-09-17 06:52:28 +0700

    Update 32.g null_assertion.dart

--- 32.g null_assertion.dart
+++ 32.g null_assertion.dart
@@ -1,17 +1,8 @@
 void main() {
   String? token = "abc";
 
-  // ✅ Cách 1: Dùng !
   print(token!.length); // 3
 
   token = null;
   // print(token!.length); // Runtime error: Null check operator used on a null value
-
-  // ❌ Cách 2: Không dùng !
-  String? token2 = "abc";
-  if (token2 != null) {
-    print(token2.length); // 3
-  } else {
-    throw Exception("Token is null!"); // tự xử lý thay vì crash ngầm
-  }
 }

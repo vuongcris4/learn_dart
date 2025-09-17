0eb7790153d9a3b8f9e59bba881564b54da63740
Trần Duy Vương <vuong@Vuongs-MacBook.local>
2025-09-17 07:34:23 +0700

    edit

--- 32.c null_aware_access.dart
+++ 32.c null_aware_access.dart
@@ -4,6 +4,7 @@ void main() {
   // ✅ Cách 1: Dùng ?.
   print(text?.length); // null
   print(text?.length + 5);
+  // print((text?.length ?? 0) + 5);
 
   // ❌ Cách 2: Không dùng ?.
   if (text == null) {
